96a9f01cc8575e5a04d73b75009490cfd1b11528
Oscar <oscardelafuentetoledo10@gmail.com>
2024-02-03 12:09:07 +0100

    añadido servicios

--- src/app/services/movie.service.ts
+++ src/app/services/movie.service.ts
@@ -1,9 +1,22 @@
-import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Injectable, inject } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
 
+const BASE_URL = 'https://api.themoviedb.org/3';
+const API_KEY = environment.apiKey;
 @Injectable({
   providedIn: 'root'
 })
 export class MovieService {
-
+  private http = inject(HttpClient);
   constructor() { }
+
+  getMovies(page: number = 1): Observable<any> {
+    return this.http.get(`${BASE_URL}/movie/popular?page=${page}&api_key=${API_KEY}`);
+  }
+
+  getMoviesDetail(id: string): Observable<any> {
+    return this.http.get(`${BASE_URL}/movie/${id}?api_key=${API_KEY}`);
+  }
 }
