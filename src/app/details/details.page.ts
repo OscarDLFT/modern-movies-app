806e306dbc3b89658e3e08d8e8574412e9489ee8
Oscar <oscardelafuentetoledo10@gmail.com>
2024-02-03 17:51:01 +0100

    añadido defer a la hora de cargar y detalles de la la pelicula

--- src/app/details/details.page.ts
+++ src/app/details/details.page.ts
@@ -1,20 +1,62 @@
-import { Component, OnInit } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { Component, Input, OnInit, WritableSignal, signal } from '@angular/core';
+import { CommonModule, CurrencyPipe, DatePipe } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { IonicModule } from '@ionic/angular';
+import { MovieService } from '../services/movie.service';
+import { MovieResult } from '../services/interfaces';
+import { addIcons } from 'ionicons';
+import { cashOutline, calendarOutline } from 'ionicons/icons';
+import { IonBackButton, IonButtons, IonCard, IonCardContent, IonCardHeader, IonCardSubtitle, IonCardTitle, IonContent, IonHeader, IonIcon, IonItem, IonLabel, IonList, IonText, IonTitle, IonToolbar } from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-details',
   templateUrl: './details.page.html',
   styleUrls: ['./details.page.scss'],
   standalone: true,
-  imports: [IonicModule, CommonModule, FormsModule]
+  imports: [
+    // IonicModule, 
+    CommonModule, 
+    FormsModule,
+    IonHeader, 
+    IonToolbar, 
+    IonTitle, 
+    IonContent, 
+    IonList, 
+    IonItem, 
+    IonLabel,
+    DatePipe,
+    CurrencyPipe,
+    IonText,
+    IonButtons,
+    IonBackButton,
+    IonCard,
+    IonCardHeader,
+    IonCardSubtitle,
+    IonCardTitle,
+    IonCardContent,
+    IonIcon,
+  ]
 })
 export class DetailsPage implements OnInit {
+  // VARIABLES
+  public imageBaseUrl: string = 'https://image.tmdb.org/t/p';
+  public movie: WritableSignal<MovieResult | null> = signal(null);
 
-  constructor() { }
+  // INPUTS
+  @Input() 
+  set id(movieId: string) {
+    this._movieService.getMoviesDetail(movieId)
+    .subscribe((movie) => {
+      this.movie.set(movie);
+    })
+  }
 
-  ngOnInit() {
+  constructor(
+    private _movieService: MovieService,
+  ) { }
+
+  ngOnInit(): void {
+    addIcons({cashOutline, calendarOutline});
   }
 
 }
--- src/app/services/interfaces.ts
+++ src/app/services/interfaces.ts
@@ -20,4 +20,7 @@ export interface MovieResult {
 	video: boolean;
 	vote_average: number;
 	vote_count: number;
+	tagline: string;
+	genres: any; 
+	budget: number;
 }
