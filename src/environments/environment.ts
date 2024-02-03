5d4454636d6d74ce98c867ad6513fd2bc23ffb29
Oscar <oscardelafuentetoledo10@gmail.com>
2024-02-03 11:49:11 +0100

    añadida apikey

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,8 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  apiKey: '6bf66cda161371c324b2442e9b4f0683'
 };
 
 /*
